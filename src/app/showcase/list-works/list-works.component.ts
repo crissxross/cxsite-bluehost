7e28d99089e26ad79a304b4fc0ed3c0a5b957e37
Christine Wilks <crissxross@gmail.com>
2016-07-13 14:58:36 +0100

    add more info to listWorks + minor edits

--- src/app/showcase/list-works/list-works.component.ts
+++ src/app/showcase/list-works/list-works.component.ts
@@ -1,13 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, trigger, state, style, animate, transition } from '@angular/core';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
+
+import { CollapseToggleDirective } from '../../shared/collapse-toggle.directive';
 
 @Component({
   moduleId: module.id,
   selector: 'app-list-works',
   templateUrl: 'list-works.component.html',
   styleUrls: ['list-works.component.css'],
-  directives: [MD_BUTTON_DIRECTIVES, MD_CARD_DIRECTIVES]
+  directives: [MD_BUTTON_DIRECTIVES, MD_CARD_DIRECTIVES, MD_LIST_DIRECTIVES, CollapseToggleDirective],
+  animations: [
+    trigger('panelState', [
+      state('open', style({opacity: 1, height: '*' })),
+      transition('void => *', [
+        style({opacity: 0, height: '0' }),
+        animate('200ms ease-in-out')
+      ]),
+      transition('* => void', [
+        animate('200ms ease-in-out', style({opacity: 0, height: 0 }))
+      ])
+    ])
+  ]
 })
 export class ListWorksComponent implements OnInit {
 
