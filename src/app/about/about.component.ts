1c95dfa754c15e945a11790c4b97171d26bbba20
Christine Wilks <crissxross@gmail.com>
2018-02-21 10:23:40 +0000

    minor edits & update cdk & material@5.2.2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { MatCard, MatCardContent, MatCardTitle, MatCardSubtitle } from '@angular/material';
 
 @Component({
   selector: 'app-about',
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { MatSidenavContainer, MatSidenav, MatList, MatNavList, MatListItem, MatDivider, MatIcon} from '@angular/material';
 
 @Component({
   selector: 'app-root',
