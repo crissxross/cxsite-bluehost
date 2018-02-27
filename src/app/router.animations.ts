fab4e87f0e2ee58b406fcfe2d13ba7912b8b513d
Christine Wilks <crissxross@gmail.com>
2018-02-27 13:29:02 +0000

    route transition animations (fade)

--- src/app/router.animations.ts
+++ src/app/router.animations.ts
@@ -1,38 +1,81 @@
-import {trigger, animate, style, group, query, transition} from '@angular/animations';
+import { trigger, animate, style, group, query, transition } from '@angular/animations';
 
-// original animation from gsans article/plunker
-// https://plnkr.co/edit/IDwAmHkzH07VbU0Bsxcj?p=preview
+// FADE
 export const routerTransition = trigger('routerTransition', [
   transition('* <=> *', [
-    query(':enter, :leave', style({ position: 'fixed', width: '100%' })
+    query(':enter, :leave', style({ display: 'grid', opacity: 0 })
       , { optional: true }),
     group([
       query(':enter', [
-        style({ transform: 'translateX(100%)' }),
-        animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
+        style({ opacity: 0 }),
+        animate('0.5s ease-in-out', style({ opacity: 1 }))
       ], { optional: true }),
       query(':leave', [
-        style({ transform: 'translateX(0%)' }),
-        animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' }))
+        style({ opacity: 1 }),
+        animate('0.5s ease-in-out', style({ opacity: 0 }))
       ], { optional: true }),
     ])
   ])
 ])
 
+// SLIDE
+// export const routerTransition = trigger('routerTransition', [
+//   transition('* <=> *', [
+//     query(':enter, :leave', style({ display: 'grid' }), { optional: true }),
+//     group([
+//       query(':enter', [
+//         style({ transform: 'translateX(100%)' }),
+//         animate('0.5s ease-out', style({ transform: 'translateX(0%)' }))
+//       ], { optional: true }),
+//       query(':leave', [
+//         style({ transform: 'translateX(0%)' }),
+//         animate('0.5s ease-out', style({ transform: 'translateX(-100%)' }))
+//       ], { optional: true }),
+//     ])
+//   ])
+// ])
 
-export const routerTestTransition = trigger('routerTestTransition', [
-  transition('* <=> *', [
-    query(':enter, :leave', style({ width: '100%' })
-      , { optional: true }),
-    group([
-      query(':enter', [
-        style({ transform: 'translate3d(100%, 0, 0)' }),
-        animate('0.5s ease-in-out', style({ transform: 'translate3d(0%, 0, 0)' }))
-      ], { optional: true }),
-      query(':leave', [
-        style({ transform: 'translate3d(100%, 0, 0)' }),
-        animate('0.5s ease-in-out', style({ transform: 'translate3d(-100%, 0, 0)' }))
-      ], { optional: true }),
-    ])
-  ])
-])
+
+// -----------------------------------
+
+// original animation from gsans Medium article & plunker
+// Angular — Supercharge your Router transitions using new animation features (v4.3+)
+// https://plnkr.co/edit/IDwAmHkzH07VbU0Bsxcj?p=preview
+
+// export const routerTransition = trigger('routerTransition', [
+//   transition('* <=> *', [
+//     query(':enter, :leave', style({ position: 'fixed', width: '100%' })
+//       , { optional: true }),
+//     group([
+//       query(':enter', [
+//         style({ transform: 'translateX(100%)' }),
+//         animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
+//       ], { optional: true }),
+//       query(':leave', [
+//         style({ transform: 'translateX(0%)' }),
+//         animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' }))
+//       ], { optional: true }),
+//     ])
+//   ])
+// ])
+
+// --------------------------------------------
+
+
+// this attempt based on gsans but influenced by Material2/tabs-animations.ts
+// export const routerTestTransition = trigger('routerTestTransition', [
+//   transition('* <=> *', [
+//     query(':enter, :leave', style({ width: '100%' })
+//       , { optional: true }),
+//     group([
+//       query(':enter', [
+//         style({ transform: 'translate3d(100%, 0, 0)' }),
+//         animate('0.5s ease-in-out', style({ transform: 'translate3d(0%, 0, 0)' }))
+//       ], { optional: true }),
+//       query(':leave', [
+//         style({ transform: 'translate3d(100%, 0, 0)' }),
+//         animate('0.5s ease-in-out', style({ transform: 'translate3d(-100%, 0, 0)' }))
+//       ], { optional: true }),
+//     ])
+//   ])
+// ])
